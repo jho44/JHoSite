a679b4a02af788eced81f51f78521be23e500a9d
Jessica Ho <jho@Jessicas-MacBook-Air.local>
2020-03-23 14:14:06 -0700

    put me.jpg in same folder + try to fix portrait->landscape

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -13,17 +13,38 @@ import MediaQuery from "react-responsive"
 
 const images = [];
 var BioPic = null;
+const me = require("./me.jpg")
 
 export default class HomePage extends Component {
 
     constructor() {
         super()
+        const isPortrait = () => {
+            const heightOutput = document.documentElement.clientHeight;
+            const widthOutput = document.documentElement.clientWidth;
+            return heightOutput >= widthOutput
+        }    
+        
         this.state = {
             data: [],
-            isFlipped: false
+            isFlipped: false,
+            orientation: isPortrait() ? "portrait" : "landscape"
         }
+        // Dimensions.addEventListener("change", () => {
+        //     this.setState({
+        //         orientation: isPortrait() ? "portrait" : "landscape"
+        //     })
+        // })
+        window.addEventListener("resize", () => {
+            this.setState({
+                orientation: isPortrait() ? "portrait" : "landscape"
+            })
+        });
+
     }
 
+
+    
     componentDidMount(){
         AOS.init({
             duration : 1500
@@ -55,35 +76,60 @@ export default class HomePage extends Component {
     }
   
     showFront() {
-    this.setState({
-        isFlipped: false
-    });
+        this.setState({
+            isFlipped: false
+        });
     }
 
-    getBioPic(data) {
-        for (const index in data) {
-            let alt = data[index] && data[index]["alt"]
-            if (alt==="me")
-            {
-                BioPic=data[index]["hyperlink"]
-                break
-            }
-        }
-    }
+
+    // getBioPic(data) {
+    //     for (const index in data) {
+    //         let alt = data[index] && data[index]["alt"]
+    //         if (alt==="me")
+    //         {
+    //             BioPic=data[index]["hyperlink"]
+    //             break
+    //         }
+    //     }
+    // }
 
     render() {
         const { data } = this.state
         this.fillImageArr(data, images)
-        this.getBioPic(data)
-        return (
-            <Container>
-                <head>
-                <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat" />
-                <meta name="viewport" content="width=device-width,initial-scale=1"/>
-                </head>
-                <TopBar history={this.props.history}/>
-                <MediaQuery query = "(orientation: portrait)">
-                    <Desktop>
+        if (this.state.orientation === "landscape") {
+            return(
+                <Container>
+                    <head>
+                    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat" />
+                    <meta name="viewport" content="width=width, initial-scale=1, minimum-scale=1" />
+
+                    </head>
+                    <TopBar history={this.props.history}/>
+                    
+                    <MediaQuery query = "(max-width: 896px)">
+                        <BetterHeader style={{height: "90vh"}}>
+                            <HeaderText data-aos="fade-right" style={{fontFamily: "Girassol", fontSize: "2.5rem", padding: "0.5rem"}}>J. Ho</HeaderText>
+                            <HeaderText data-aos="fade-right" data-aos-delay="400" style={{fontFamily: "Dancing Script", fontSize: "2.5rem", padding: 0}}>Off</HeaderText>
+                            <HeaderText data-aos="fade-right" daa-aos-delay="800" style={{fontFamily: "Abel", fontSize: "1rem", padding: "0.8rem", paddingBottom: 0}}>the</HeaderText>
+                            <HeaderText data-aos="fade-right" data-aos-delay="1000" data-aos-anchor-placement="bottom-bottom" style={{fontFamily: "Courier Prime", fontSize: "2.6rem", padding: "0.5rem"}}>Clock</HeaderText>
+                        </BetterHeader>
+                        <BetterAboutMe style={{padding: "2rem 0"}}>
+                        <FlipCard>
+                                <Front>
+                                    <ImageThing style={{height: "100%", borderRadius: "10px 0 0 10px"}} image={me}/>
+                                    <TextFrontArea style={{flexGrow: 0.4}}>
+                                        <TextFront>Maximum</TextFront>
+                                        <TextFront>Effort</TextFront>
+                                        <TextFront>J. Ho</TextFront>
+                                    </TextFrontArea>
+                                </Front>
+                                <Back>
+                                    I'm a UCLA CS Major. I got bored one day and decided to create this website to keep track of my progress on some projects and showcase my past works.
+                                </Back>
+                            </FlipCard>
+                        </BetterAboutMe>
+                    </MediaQuery>
+                    <MediaQuery query = "(min-width: 1025px)">
                         <UpArrowBtn/>
                         <BetterHeader style={{height: "90vh"}}>
                             <HeaderText data-aos="fade-right" style={{fontFamily: "Girassol", fontSize: "4rem"}} >J. Ho</HeaderText>
@@ -94,7 +140,7 @@ export default class HomePage extends Component {
                         <BetterAboutMe>
                             <FlipCard>
                                 <Front>
-                                    <ImageThing style={{height: "100%", borderRadius: "10px 0 0 10px", }} image={BioPic}/>
+                                    <ImageThing style={{height: "100%", borderRadius: "10px 0 0 10px", }} image={me}/>
                                     <TextFrontArea style={{flexGrow: 0.3}}>
                                         <TextFront>Maximum</TextFront>
                                         <TextFront>Effort</TextFront>
@@ -106,8 +152,44 @@ export default class HomePage extends Component {
                                 </Back>
                             </FlipCard>
                         </BetterAboutMe>
-                    </Desktop>
+                    {/* </Desktop> */}
+                    </MediaQuery>
+                    <Medium>
+                        <BetterHeader style={{height: "90vh"}}>
+                            <HeaderText data-aos="fade-right" style={{fontFamily: "Girassol", fontSize: "4rem"}}>J. Ho</HeaderText>
+                            <HeaderText data-aos="fade-right" data-aos-delay="400" style={{fontFamily: "Dancing Script", fontSize: "3.5rem"}}>Off</HeaderText>
+                            <HeaderText data-aos="fade-right" daa-aos-delay="800" style={{fontFamily: "Abel", fontSize: "2rem"}}>the</HeaderText>
+                            <HeaderText data-aos="fade-right" data-aos-delay="1200" style={{fontFamily: "Courier Prime", fontSize: "3.6rem"}}>Clock</HeaderText>
+                        </BetterHeader>
+                        <BetterAboutMe style={{padding: "2rem 0"}}>
+                        <FlipCard>
+                                <Front>
+                                    <ImageThing style={{height: "100%", borderRadius: "10px 0 0 10px", backgroundPosition: "top center"}} image={me}/>
+                                    <TextFrontArea style={{flexGrow: 0.4}}>
+                                        <TextFront>Maximum</TextFront>
+                                        <TextFront>Effort</TextFront>
+                                        <TextFront>J. Ho</TextFront>
+                                    </TextFrontArea>
+                                </Front>
+                                <Back>
+                                    I'm a UCLA CS Major. I got bored one day and decided to create this website to keep track of my progress on some projects and showcase my past works.
+                                </Back>
+                            </FlipCard>
+                        </BetterAboutMe>
+                    </Medium>
+                </Container>
+            )
+        }
+        else {
+            return (
+                <Container>
+                    <head>
+                    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat" />
+                    <meta name="viewport" content="width=width, initial-scale=1, minimum-scale=1" />
 
+                    </head>
+                    <TopBar history={this.props.history}/>
+                    
                     <MobilePortrait>
                         <BetterHeader style={{height: "95vh"}}>
                             <HeaderText data-aos="fade-right" style={{fontFamily: "Girassol", fontSize: "3.5rem"}}>J. Ho</HeaderText>
@@ -118,7 +200,7 @@ export default class HomePage extends Component {
                         <BetterAboutMe>
                             <FlipCard>
                                 <MobileFront>
-                                    <ImageThing style={{marginTop: "5%", marginBottom: "4%", width: "80%"}} image={BioPic}/>
+                                    <ImageThing style={{marginTop: "5%", marginBottom: "4%", width: "80%"}} image={me}/>
                                     <TextFrontArea>
                                         <TextFront>Maximum</TextFront>
                                         <TextFront>Effort</TextFront>
@@ -131,33 +213,6 @@ export default class HomePage extends Component {
                             </FlipCard>
                         </BetterAboutMe>
                     </MobilePortrait>
-                    
-                    <Medium>
-                        <BetterHeader style={{height: "90vh"}}>
-                            <HeaderText data-aos="fade-right" style={{fontFamily: "Girassol", fontSize: "4rem"}}>J. Ho</HeaderText>
-                            <HeaderText data-aos="fade-right" data-aos-delay="400" style={{fontFamily: "Dancing Script", fontSize: "3.5rem"}}>Off</HeaderText>
-                            <HeaderText data-aos="fade-right" daa-aos-delay="800" style={{fontFamily: "Abel", fontSize: "2rem"}}>the</HeaderText>
-                            <HeaderText data-aos="fade-right" data-aos-delay="1200" style={{fontFamily: "Courier Prime", fontSize: "3.6rem"}}>Clock</HeaderText>
-                        </BetterHeader>
-                        <BetterAboutMe style={{padding: "2rem 0"}}>
-                        <FlipCard>
-                                <Front>
-                                    <ImageThing style={{height: "100%", borderRadius: "10px 0 0 10px", backgroundPosition: "top center"}} image={BioPic}/>
-                                    <TextFrontArea style={{flexGrow: 0.4}}>
-                                        <TextFront>Maximum</TextFront>
-                                        <TextFront>Effort</TextFront>
-                                        <TextFront>J. Ho</TextFront>
-                                    </TextFrontArea>
-                                </Front>
-                                <Back>
-                                    I'm a UCLA CS Major. I got bored one day and decided to create this website to keep track of my progress on some projects and showcase my past works.
-                                </Back>
-                            </FlipCard>
-                        </BetterAboutMe>
-                    </Medium>
-                </MediaQuery>
-
-                <MediaQuery query = "(orientation: landscape)">
                     <Desktop>
                         <UpArrowBtn/>
                         <BetterHeader style={{height: "90vh"}}>
@@ -169,7 +224,7 @@ export default class HomePage extends Component {
                         <BetterAboutMe>
                             <FlipCard>
                                 <Front>
-                                    <ImageThing style={{height: "100%", borderRadius: "10px 0 0 10px", }} image={BioPic}/>
+                                    <ImageThing style={{height: "100%", borderRadius: "10px 0 0 10px", }} image={me}/>
                                     <TextFrontArea style={{flexGrow: 0.3}}>
                                         <TextFront>Maximum</TextFront>
                                         <TextFront>Effort</TextFront>
@@ -192,7 +247,7 @@ export default class HomePage extends Component {
                         <BetterAboutMe style={{padding: "2rem 0"}}>
                         <FlipCard>
                                 <Front>
-                                    <ImageThing style={{height: "100%", borderRadius: "10px 0 0 10px", backgroundPosition: "top center"}} image={BioPic}/>
+                                    <ImageThing style={{height: "100%", borderRadius: "10px 0 0 10px", backgroundPosition: "top center"}} image={me}/>
                                     <TextFrontArea style={{flexGrow: 0.4}}>
                                         <TextFront>Maximum</TextFront>
                                         <TextFront>Effort</TextFront>
@@ -205,33 +260,9 @@ export default class HomePage extends Component {
                             </FlipCard>
                         </BetterAboutMe>
                     </Medium>
-                    <MobileLandscape>
-                        <BetterHeader style={{height: "90vh"}}>
-                            <HeaderText data-aos="fade-right" style={{fontFamily: "Girassol", fontSize: "2.5rem", padding: "0.5rem"}}>J. Ho</HeaderText>
-                            <HeaderText data-aos="fade-right" data-aos-delay="400" style={{fontFamily: "Dancing Script", fontSize: "2.5rem", padding: 0}}>Off</HeaderText>
-                            <HeaderText data-aos="fade-right" daa-aos-delay="800" style={{fontFamily: "Abel", fontSize: "1rem", padding: "0.8rem", paddingBottom: 0}}>the</HeaderText>
-                            <HeaderText data-aos="fade-right" data-aos-delay="1000" data-aos-anchor-placement="bottom-bottom" style={{fontFamily: "Courier Prime", fontSize: "2.6rem", padding: "0.5rem"}}>Clock</HeaderText>
-                        </BetterHeader>
-                        <BetterAboutMe style={{padding: "2rem 0"}}>
-                        <FlipCard>
-                                <Front>
-                                    <ImageThing style={{height: "100%", borderRadius: "10px 0 0 10px"}} image={BioPic}/>
-                                    <TextFrontArea style={{flexGrow: 0.4}}>
-                                        <TextFront>Maximum</TextFront>
-                                        <TextFront>Effort</TextFront>
-                                        <TextFront>J. Ho</TextFront>
-                                    </TextFrontArea>
-                                </Front>
-                                <Back>
-                                    I'm a UCLA CS Major. I got bored one day and decided to create this website to keep track of my progress on some projects and showcase my past works.
-                                </Back>
-                            </FlipCard>
-                        </BetterAboutMe>
-                    </MobileLandscape>
-                </MediaQuery>
-            </Container>
-
-        )
+                </Container>
+            )
+        }
     }
 }
 
